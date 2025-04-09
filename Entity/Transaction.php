c632145e61c06064f250c010a8b7c35317d7551c
Alexey Verenikin <2457824+alexver@users.noreply.github.com>
2025-04-09 22:33:35 +0300

    symfony 6 & php 8 support

--- Entity/Transaction.php
+++ Entity/Transaction.php
@@ -2,13 +2,10 @@
 
 namespace Beelab\PaypalBundle\Entity;
 
+use DateTime;
 use Doctrine\ORM\Mapping as ORM;
 
-/**
- * Transaction.
- *
- * @ORM\MappedSuperclass
- */
+#[ORM\MappedSuperclass]
 abstract class Transaction
 {
     const STATUS_KO = -1;
@@ -23,56 +20,28 @@ abstract class Transaction
         self::STATUS_ERROR => 'failed',
     ];
 
-    /**
-     * @var int
-     *
-     * @ORM\Column(type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    protected $id;
+    #[ORM\Id]
+    #[ORM\Column(type: "integer")]
+    #[ORM\GeneratedValue(strategy: "AUTO")]
+    protected int $id;
 
-    /**
-     * @var \DateTime
-     *
-     * @ORM\Column(type="datetime")
-     */
-    protected $start;
+    #[ORM\Column(type: "datetime")]
+    protected DateTime $start;
 
-    /**
-     * @var \DateTime
-     *
-     * @ORM\Column(type="datetime", nullable=true)
-     */
-    protected $end;
+    #[ORM\Column(type: "datetime", nullable: true)]
+    protected DateTime $end;
 
-    /**
-     * @var int
-     *
-     * @ORM\Column(type="smallint", options={"default": 0})
-     */
-    protected $status = self::STATUS_STARTED;
+    #[ORM\Column(type: "smallint", options: ["default" => 0])]
+    protected int $status = self::STATUS_STARTED;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(unique=true)
-     */
-    protected $token;
+    #[ORM\Column(type: "string", unique: true)]
+    protected string $token;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(type="decimal", precision=6, scale=2, options={"default": 0})
-     */
-    protected $amount = 0;
+    #[ORM\Column(type: "decimal", precision: 6, scale: 2, options: ["default" => 0])]
+    protected float $amount = 0.0;
 
-    /**
-     * @var array
-     *
-     * @ORM\Column(type="array")
-     */
-    protected $response;
+    #[ORM\Column(type: "array")]
+    protected array $response = [];
 
     public function __construct($amount = null)
     {
